a07bf45bef3457e264333dda424aab87aaf4bb60
Filip Chalupa <chalupa.filip@gmail.com>
2023-06-25 15:08:51 +0200

    Fix pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+	base: '/templating-demo', // Nutno upravit podle názvu repozitáře
 	plugins: [react()],
 })
