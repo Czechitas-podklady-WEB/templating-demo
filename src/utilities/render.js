bb9e0dc68ba055bb8272f6caf6085ee59900a6fb
Filip Chalupa <chalupa.filip@gmail.com>
2023-06-25 14:52:32 +0200

    Render: add Jsdoc

--- src/utilities/render.js
+++ src/utilities/render.js
@@ -1,5 +1,9 @@
 import { renderToString } from 'react-dom/server'
 
+/**
+ * @param {HTMLElement} rootElement
+ * @param {ReactElement} reactNode
+ */
 export const render = (rootElement, reactNode) => {
 	const html = renderToString(reactNode)
 
