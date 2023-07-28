f48453a570035a6ca76ee7d22ca63306adec6008
Filip Chalupa <chalupa.filip@gmail.com>
2023-07-28 17:39:25 +0200

    Vite: simplify input

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { czechitasRenderVitePlugin } from '@czechitas/render/plugin'
 import { glob } from 'glob'
-import { extname, relative } from 'path'
-import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -12,14 +10,7 @@ export default defineConfig({
 		emptyOutDir: true,
 		target: 'es2022',
 		rollupOptions: {
-			input: Object.fromEntries(
-				glob
-					.sync('src/**/*.html')
-					.map((file) => [
-						relative('src', file.slice(0, file.length - extname(file).length)),
-						fileURLToPath(new URL(file, import.meta.url)),
-					]),
-			),
+			input: glob.sync('src/**/*.html'),
 		},
 	},
 	plugins: [czechitasRenderVitePlugin()],
